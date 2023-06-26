75ef39f34576c0ff3c43e49578c562ad381ae8ce
nrzngr <109433343+nrzngr@users.noreply.github.com>
2023-06-26 08:16:54 +0700

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -3,11 +3,13 @@
 const api_key = '8367b5b226d32b7bcd07f585ee724277';
 const imageBaseURL = 'https://image.tmdb.org/t/p/';
 
+
 /**
- * fetch data from a server using the `url` and passes
- * the result in JSON data to a `callback` function,
- * along with an optional parameter if has `optionalParam`.
+ * fetch data dari server menggunakan `url` dan menaruh
+ * hasilnya kedalam data berbentuk JSON ke dalam `callback`,
+ * bersamaan dengan parameter optional (jika ada) ke dalam `optionalParam`.
 */
+
 const fetchDataFromServer = function(url, callback, optionalParam) {
   fetch(url)
     .then(response => response.json())
